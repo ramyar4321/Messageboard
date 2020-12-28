ef95809abe1b3f7612a307186272f1d53dd45b32
g7x9a <g7x9a@ugrad.cs.ubc.ca>
2020-12-28 09:07:47 -0800

    Model message fields that are not required have default value of undefined

--- backend/server/models/message.model.js
+++ backend/server/models/message.model.js
@@ -2,8 +2,8 @@ import mongoose, { Schema } from 'mongoose'
 
 const message = new Schema({
     title: {type: String, required: true, unique: true},
-    message: {type: String, required: false},
-    image: {type: String, required: false},
+    message: {type: String, required: false, default: undefined},
+    image: {type: String, required: false, default: undefined},
     createdAt: {type: Date, default: new Date()}
 })
 
